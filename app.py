9317d299b4613846c917b0d2d3028b2ee64408b5
tanaka daiki <d.tanaka.sys20@morijyobi.ac.jp>
2021-03-30 12:08:38 +0900

    計算アプリの新規作成を行いました。
    給与アプリ作成をお願いいたします。

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("test")
+from flask import Flask,request,redirect,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def top():
+    return render_template("index.html")
+
+@app.route("/math")
+def math():
+    return render_template("math.html")
+
+@app.route("/math2")
+def calc_yen():
+    hankei = float(request.args.get("hankei"))
+    ensyu = float((hankei * 2)* 3.14)
+    menseki = float(hankei * hankei * 3.14)
+    return render_template("math2.html",ensyu=ensyu,menseki=menseki)
+
+if __name__ == "__main__":
+    app.run(debug=True)
